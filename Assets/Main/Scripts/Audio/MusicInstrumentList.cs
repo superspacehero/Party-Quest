fc6ed6a630fa764ec68d5503e0b930130ce573d9
Asher Pinson <ashpinson@gmail.com>
2022-12-16 17:16:09 -0500

    Further music work

--- Assets/Main/Scripts/Audio/MusicInstrumentList.cs
+++ Assets/Main/Scripts/Audio/MusicInstrumentList.cs
@@ -1,22 +1,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-    // A class representing an instrument in the song
-    [System.Serializable, CreateAssetMenu(fileName = "New Music Instrument List", menuName = "Music Instrument List")]
-    public class MusicInstrumentList : ScriptableObject
+// A class representing an instrument in the song
+[System.Serializable, CreateAssetMenu(fileName = "New Music Instrument List", menuName = "Music Instrument List")]
+public class MusicInstrumentList : ScriptableObject
+{
+    public List<MusicInstrument> instruments = new List<MusicInstrument>();
+
+    public MusicInstrument GetInstrument(string name)
     {
-        public List<MusicInstrument> instruments;
-
-        public MusicInstrument GetInstrument(string name)
-        {
-            foreach (MusicInstrument instrument in instruments)
-            {
-                if (instrument.name == name)
-                {
-                    return instrument;
-                }
-            }
-
-            return null;
-        }
+        return instruments.Find(instrument => instrument.name == name);
     }
+}
+
+[System.Serializable]
+public class MusicInstrument
+{
+    // The name of the instrument
+    public string name;
+
+    // The sample to be played by the instrument
+    public AudioClip sample;
+
+    // The original key
+    public MusicSequencer.Note originalKey;
+
+    // Whether or not the instrument loops
+    public bool loop;
+}
--- Assets/Main/Scripts/Audio/MusicSequencer.cs
+++ Assets/Main/Scripts/Audio/MusicSequencer.cs
@@ -1,20 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
+using Sirenix.OdinInspector;
 
 public class MusicSequencer : MonoBehaviour
 {
     // The JSON string containing the song information
-    public string jsonSong;
-
-    // The index of the note to loop at
-    private int loopIndex;
+    [TextArea(10, 20)] public string jsonSong;
 
     // The song data
-    private SongData songData;
+    private SongData songData = new SongData();
 
     // The list of instruments
     [SerializeField] private MusicInstrumentList instruments;
+
+    // The AudioMixerGroup to which the audio sources will be assigned
+    [SerializeField] private AudioMixerGroup audioMixerGroup;
 
     void Start()
     {
@@ -24,38 +26,68 @@ public class MusicSequencer : MonoBehaviour
 
     // A class representing the data in the JSON file
     [System.Serializable]
-    private class SongData
+    public class SongData
     {
         public string name;
         public float tempo;
         public int loop;
         public List<Track> tracks;
+
+        public SongData()
+        {
+            name = "Placeholder";
+            tempo = 120f;
+            loop = 0;
+            tracks = new List<Track> { new Track() };
+        }
     }
+
 
     [System.Serializable]
     public class Note
     {
-        public char note;
+        [PropertyRange(1, 12)] public int note;
         public int octave;
 
-        public Note(char note, int? octave)
+        public Note(int note, int? octave)
         {
             this.note = note;
             this.octave = octave ?? 0;
         }
 
-        public Note(int note, int octave)
+        // Use Odin Inspector to make a custom property drawer for the note, allowing GetNoteName to be shown in the inspector
+        [ShowInInspector, PropertyOrder(-1)]
+        public string noteLetter
         {
-            // Convert the note to a hex character
-            this.note = int.Parse(note.ToString(), System.Globalization.NumberStyles.HexNumber).ToString("X")[0];
-            this.octave = octave;
+            get
+            {
+                switch (note)
+                {
+                    case 1: return "C";
+                    case 2: return "C#";
+                    case 3: return "D";
+                    case 4: return "D#";
+                    case 5: return "E";
+                    case 6: return "F";
+                    case 7: return "F#";
+                    case 8: return "G";
+                    case 9: return "G#";
+                    case 10: return "A";
+                    case 11: return "A#";
+                    case 12: return "B";
+                    default: return "";
+                }
+            }
         }
     }
 
     // A class representing a track in the song
     [System.Serializable]
-    private class Track
+    public class Track
     {
+        // Whether or not to play the track
+        public bool play = true;
+
         // The track's instrument
         public MusicInstrument instrument;
 
@@ -69,7 +101,16 @@ public class MusicSequencer : MonoBehaviour
         // A dictionary storing the notes and their pitches
         [System.NonSerialized]
         public List<Note> notes;
+
+        public Track()
+        {
+            instrument = new MusicInstrument();
+            audioSource = null;
+            noteString = "";
+            notes = new List<Note>();
+        }
     }
+
 
     // Parses the JSON file and initializes the song's properties
     private bool ParseJSON(string jsonString)
@@ -79,10 +120,10 @@ public class MusicSequencer : MonoBehaviour
 
         // Read the JSON string and create a SongData object
         jsonSong = jsonString;
-        SongData newSongData = JsonUtility.FromJson<SongData>(jsonString);
+        songData = JsonUtility.FromJson<SongData>(jsonString);
 
-        // Initialize the song's properties
-        songData = newSongData;
+        // Remove the existing audio sources
+        DestroyAudioSources();
 
         // Create an audio source for each track
         foreach (Track track in songData.tracks)
@@ -97,11 +138,13 @@ public class MusicSequencer : MonoBehaviour
             for (int i = 0; i < track.noteString.Length; i++)
             {
                 // Get the current note
-                Note note = new Note(track.noteString[i], null);
+                Note note = new Note(0, null);
 
                 // Calculate the pitch of the note if it is not a space or dash
-                if (note.note != ' ' && note.note != '-')
+                if (track.noteString[i] != ' ' && track.noteString[i] != '-' && track.noteString[i] != '.')
                 {
+                    note.note = System.Convert.ToInt32(track.noteString[i].ToString(), 16);
+
                     i++;
                     int octave = int.Parse(track.noteString[i].ToString());
 
@@ -110,6 +153,13 @@ public class MusicSequencer : MonoBehaviour
                 }
                 else
                 {
+                    switch (track.noteString[i])
+                    {
+                        case '-': note.note = 0; break;
+                        case '.': note.note = -1; break;
+                        case ' ': continue;
+                    }
+
                     // Add the rest/hold as a null value in the dictionary
                     track.notes.Add(note);
                 }
@@ -120,14 +170,15 @@ public class MusicSequencer : MonoBehaviour
 
             // Set the audio source's properties
             audioSource.clip = track.instrument.sample;
-            audioSource.loop = true;
+            audioSource.loop = track.instrument.loop;
             audioSource.playOnAwake = false;
+            audioSource.outputAudioMixerGroup = audioMixerGroup;
 
             // Assign the audio source to the track
             track.audioSource = audioSource;
         }
 
-        return true;
+        return songData != null;
     }
 
     private bool ParseJSON(TextAsset jsonFile)
@@ -140,130 +191,193 @@ public class MusicSequencer : MonoBehaviour
         return ParseJSON(jsonSong);
     }
 
+    private string GetJSON()
+    {
+        return JsonUtility.ToJson(songData);
+    }
+
     // Plays the notes in the tracks
     private IEnumerator PlayNotes()
     {
         // Parse the JSON file and initialize the song's properties
-        if (string.IsNullOrEmpty(jsonSong) && !ParseJSON())
+        if (string.IsNullOrEmpty(jsonSong) || !ParseJSON())
+        {
+            songData = new SongData();
+            jsonSong = GetJSON();
+
+            Debug.LogError("Something's wrong with the JSON file!");
             yield break;
+        }
+
+        // Return early if the instruments field is empty
+        if (instruments == null || instruments.instruments.Count == 0)
+        {
+            Debug.LogError("No instruments have been added to the MusicSequencer!");
+            yield break;
+        }
+
+        // Get the longest number of beats from the tracks
+        int loopLength = 0;
+        foreach (Track track in songData.tracks)
+        {
+            if (track.notes.Count > loopLength)
+                loopLength = track.notes.Count;
+        }
+
+        Debug.Log
+        (
+            "Playing song: " + songData.name + "\n" +
+            "Tempo: " + songData.tempo + "\n" +
+            "Loop length: " + loopLength + "\n" +
+            "Tracks: " + songData.tracks.Count + "\n\n" +
+            "JSON: " + jsonSong
+        );
 
         // Calculate the duration of a beat based on the song's tempo
-        float beatDuration = 60.0f / songData.tempo;
+        float beatDuration = (60.0f / songData.tempo);
 
-        WaitForSeconds beat = new WaitForSeconds(beatDuration);
+        WaitForSecondsRealtime beat = new WaitForSecondsRealtime(beatDuration);
 
         // Keep track of the current beat
         int currentBeat = 0;
 
         // Keep playing the song until it reaches the end
-        while (currentBeat < loopIndex)
+        while (currentBeat < loopLength)
         {
+            // Debug.Log("Beat " + (currentBeat + 1));
+
             // Play the notes in each track
             foreach (Track track in songData.tracks)
             {
-                // Parse the notes and play them
-                for (int i = 0; i < track.notes.Count; i++)
+                // If there is a note in the track at this point, play the note
+                if (!track.play || track.notes.Count <= currentBeat)
+                    continue;
+
+                // Get the current note
+                Note note = track.notes[currentBeat];
+
+                // Play the note if it is not a space or dash
+                if (note.note > 0)
                 {
-                    // Get the current note
-                    Note note = track.notes[i];
-
-                    // Play the note if it is not a space or dash
-                    if (note.note != ' ' && note.note != '-')
-                    {
-                        // Calculate the pitch of the note
-                        float pitch = CalculatePitch(note, track.instrument.originalKey);
-
-                        // Play the note
-                        track.audioSource.pitch = pitch;
-                        track.audioSource.Play();
-                    }
-                    else if (note.note == ' ')
-                    {
-                        // Stop playing the note
-                        track.audioSource.Stop();
-                    }
-
-                    // Wait for the next beat
-                    yield return beat;
+                    // Calculate the pitch of the note, and play the note
+                    track.audioSource.pitch = CalculatePitch(note, track.instrument);
+                    track.audioSource.Play();
+                }
+                else if (note.note < 0)
+                {
+                    // Stop playing the note
+                    track.audioSource.Stop();
                 }
             }
 
             // Increment the current beat
             currentBeat++;
+
+            // Wait for the next beat
+            yield return beat;
         }
     }
 
-    private float CalculatePitch(Note note, Note originalKey)
+    private float CalculatePitch(Note note, MusicInstrument instrument)
     {
-        // Calculate the pitch offset based on the original key
-        int pitchOffset = note.octave - originalKey.octave;
+        // Calculate the pitch of the note
+        float pitch = Mathf.Pow(2.0f, (note.note - instrument.originalKey.note) / 12.0f);
 
-        // Get the distance between the note and the original key
-        int noteDistance = (note.note - originalKey.note + 12) % 12;
+        // Apply the octave of the note
+        pitch *= Mathf.Pow(2.0f, note.octave - instrument.originalKey.octave);
 
-        // Calculate the pitch based on the pitch offset and the note distance
-        float pitch = 1.0f + pitchOffset + (noteDistance / 12.0f);
+        pitch *= 10;
+
+        Debug.Log("Note: " + note.note + ", pitch: " + pitch);
 
         return pitch;
     }
 
-    [Sirenix.OdinInspector.Button]
+    [Sirenix.OdinInspector.ButtonGroup("Media Buttons")]
     public void Play()
     {
+        StopCoroutine(PlayNotes());
         StartCoroutine(PlayNotes());
+    }
+
+    [Sirenix.OdinInspector.ButtonGroup("Media Buttons")]
+    public void Stop()
+    {
+        StopCoroutine(PlayNotes());
+        DestroyAudioSources();
+    }
+
+    void DestroyAudioSources()
+    {
+        // Destroy the audio sources
+        foreach (AudioSource audioSource in GetComponents<AudioSource>())
+#if UNITY_EDITOR
+            DestroyImmediate(audioSource);
+#else
+            Destroy(audioSource);
+#endif
     }
 
     #region Notes
 
-        // Sets the note at the given index in the track to the given note and octave
-        private void SetNote(int trackIndex, int noteIndex, int note, int octave)
+    // Sets the note at the given index in the track to the given note and octave
+    private void SetNote(int trackIndex, int noteIndex, int note, int octave)
+    {
+        songData.tracks[trackIndex].notes[noteIndex] = new Note(note, octave);
+    }
+
+    // Adds a new note with the given note and octave to the given track at the given index
+    private void AddNote(int trackIndex, int noteIndex, int note, int octave)
+    {
+        songData.tracks[trackIndex].notes.Insert(noteIndex, new Note(note, octave));
+    }
+
+    // Adjusts the note at the given index by the amount of semitones given
+    private void AdjustNote(int trackIndex, int noteIndex, int semitones)
+    {
+        // Get the current note and octave
+        Note currentNote = songData.tracks[trackIndex].notes[noteIndex];
+
+        // Adjust the note by the given amount of semitones
+        int newNote = currentNote.note + semitones;
+        int newOctave = currentNote.octave;
+        if (newNote > 12)
         {
-            songData.tracks[trackIndex].notes[noteIndex] = new Note(note, octave);
+            newNote -= 12;
+            newOctave++;
+        }
+        else if (newNote < 1)
+        {
+            newNote += 12;
+            newOctave--;
         }
 
-        // Adds a new note with the given note and octave to the given track at the given index
-        private void AddNote(int trackIndex, int noteIndex, int note, int octave)
+        // Set the new note and octave
+        SetNote(trackIndex, noteIndex, newNote, newOctave);
+    }
+
+    private void RaiseNote(int trackIndex, int noteIndex)
+    {
+        AdjustNote(trackIndex, noteIndex, 1);
+    }
+
+    private void LowerNote(int trackIndex, int noteIndex)
+    {
+        AdjustNote(trackIndex, noteIndex, -1);
+    }
+
+    [Button]
+    void TestAudioSources()
+    {
+        foreach (Track track in songData.tracks)
         {
-            songData.tracks[trackIndex].notes.Insert(noteIndex, new Note(note, octave));
+            if (track.audioSource != null)
+                track.audioSource.Play();
         }
 
-        // Adjusts the note at the given index in the given track up by a semitone
-        private void AdjustNoteUp(int trackIndex, int noteIndex)
-        {
-            // Get the current note and octave
-            Note currentNote = songData.tracks[trackIndex].notes[noteIndex];
-
-            // Adjust the note up by a semitone
-            int newNote = currentNote.note + 1;
-            int newOctave = currentNote.octave;
-            if (newNote > 12)
-            {
-                newNote = 1;
-                newOctave++;
-            }
-
-            // Set the new note and octave
-            SetNote(trackIndex, noteIndex, newNote, newOctave);
-        }
-
-        // Adjusts the note at the given index in the given track down by a semitone
-        private void AdjustNoteDown(int trackIndex, int noteIndex)
-        {
-            // Get the current note and octave
-            Note currentNote = songData.tracks[trackIndex].notes[noteIndex];
-
-            // Adjust the note down by a semitone
-            int newNote = currentNote.note - 1;
-            int newOctave = currentNote.octave;
-            if (newNote < 1)
-            {
-                newNote = 12;
-                newOctave--;
-            }
-
-            // Set the new note and octave
-            SetNote(trackIndex, noteIndex, newNote, newOctave);
-        }
+        foreach (AudioSource audioSource in GetComponents<AudioSource>())
+            audioSource.Play();
+    }
 
     #endregion
 }
