0aeeb75b3141b9d09b1a690aaaa07938da7bc85a
Asher Pinson <ashpinson@gmail.com>
2023-07-14 00:56:00 -0400

    CharacterSpawners (somewhat) work

--- Assets/Main/Scripts/Tiles/CharacterSpawner.cs
+++ Assets/Main/Scripts/Tiles/CharacterSpawner.cs
@@ -1,26 +1,84 @@
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
-public class CharacterSpawner : GameThing
+public class CharacterSpawner : LevelTile
 {
-    public CharacterThing.CharacterInfo characterInfo;
     public int team;
 
     /// <summary>
-    /// Start is called on the frame when a script is enabled just before
-    /// any of the Update methods is called the first time.
+    /// StartUp is called on the first frame of the running Scene.
     /// </summary>
-    void Start()
+    /// <param name="position">Position of the Tile on the Tilemap.</param>
+    /// <param name="tilemap">The Tilemap the tile is present on.</param>
+    /// <param name="instantiatedGameObject">The GameObject instantiated for the Tile.</param>
+    /// <returns>Whether StartUp was successful</returns>
+    public override bool StartUp(Vector3Int position, ITilemap tilemap, GameObject instantiatedGameObject)
     {
-        if (GameManager.instance && GameManager.instance.characterPrefab)
+        if (instantiatedGameObject != null)
+        {
+            Tilemap tmpMap = tilemap.GetComponent<Tilemap>();
+            Matrix4x4 orientMatrix = tmpMap.orientationMatrix;
+
+            Vector3 gameObjectTranslation = new Vector3();
+            Quaternion gameObjectRotation = new Quaternion();
+            Vector3 gameObjectScale = new Vector3();
+
+            bool ruleMatched = false;
+            Matrix4x4 transform = Matrix4x4.identity;
+            foreach (TilingRule rule in m_TilingRules)
+            {
+                if (RuleMatches(rule, position, tilemap, ref transform))
+                {
+                    transform = orientMatrix * transform;
+
+                    // Converts the tile's translation, rotation, & scale matrix to values to be used by the instantiated GameObject
+                    gameObjectTranslation = new Vector3(transform.m03, transform.m13 + position.z, transform.m23);
+                    gameObjectRotation = Quaternion.LookRotation(new Vector3(transform.m02, transform.m12, transform.m22), new Vector3(transform.m01, transform.m11, transform.m21));
+                    gameObjectScale = transform.lossyScale;
+
+                    ruleMatched = true;
+                    break;
+                }
+            }
+            if (!ruleMatched)
+            {
+                // Fallback to just using the orientMatrix for the translation, rotation, & scale values.
+                gameObjectTranslation = new Vector3(orientMatrix.m03, orientMatrix.m13 + position.z, orientMatrix.m23);
+                gameObjectRotation = Quaternion.LookRotation(new Vector3(orientMatrix.m02, orientMatrix.m12, orientMatrix.m22), new Vector3(orientMatrix.m01, orientMatrix.m11, orientMatrix.m21));
+                gameObjectScale = transform.lossyScale;
+            }
+
+            instantiatedGameObject.transform.localPosition = gameObjectTranslation + tmpMap.CellToLocalInterpolated(position + tmpMap.tileAnchor);
+            instantiatedGameObject.transform.localRotation = gameObjectRotation;
+            instantiatedGameObject.transform.localScale = gameObjectScale;
+
+            if (Application.isPlaying)
+                SpawnCharacter(instantiatedGameObject.transform.position);
+
+            #if !UNITY_EDITOR
+                instantiatedGameObject.isStatic = true;
+                foreach (Transform child in instantiatedGameObject.transform)
+                {
+                    child.gameObject.isStatic = true;
+                }
+            #endif
+        }
+
+        return true;
+    }
+
+    void SpawnCharacter(Vector3 position)
+    {
+        if (GameManager.instance?.characterPrefab)
         {
             if (Instantiate(GameManager.instance.characterPrefab).TryGetComponent(out CharacterThing character))
             {
-                character.characterInfo = characterInfo;
-                character.team = team;
+                character.position = position;
 
-                // Add the character to the player's input
-                AttachThing(character);
-                character.OccupyCurrentNode();
+                if (!string.IsNullOrEmpty(tileData))
+                    character.characterInfo = CharacterThing.CharacterInfo.FromString(tileData);
+                character.team = team;
+                character.position = position;
             }
         }
     }
--- Assets/Main/Scripts/Tiles/LevelTile.cs
+++ Assets/Main/Scripts/Tiles/LevelTile.cs
@@ -6,5 +6,10 @@ using UnityEngine.Tilemaps;
 [CreateAssetMenu(fileName = "New Level Tile", menuName = "2D/Tiles/Level Tile")]
 public class LevelTile : RuleTile
 {
-    public string tileData;
+    public virtual string tileData
+    {
+        get => _tileData;
+        set => _tileData = value;
+    }
+    [SerializeField] protected string _tileData;
 }
