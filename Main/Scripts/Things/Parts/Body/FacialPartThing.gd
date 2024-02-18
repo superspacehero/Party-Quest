013094ed59481e0bdba450f52e93475b0123673d
Asher Pinson <ashpinson@gmail.com>
2024-02-18 16:52:13 -0500

    👤 character: Update character shader + work on music

--- Main/Scripts/Things/Parts/Body/FacialPartThing.gd
+++ Main/Scripts/Things/Parts/Body/FacialPartThing.gd
@@ -4,14 +4,16 @@ class_name FacialPartThing
 
 @export var face_frames : Vector2i = Vector2i(4, 3):
 	set(value):
-		face_mesh.get_surface_override_material(0).Frames = value
 		face_frames = value
+		face_mesh.get_surface_override_material(0).set_shader_parameter("Frames", Vector2(face_frames.x, face_frames.y))
 
 @export var face_offset : Vector2i = Vector2i(0, 0):
 	set(value):
-		face_mesh.get_surface_override_material(0).Offset = value
-		face_offset = value
+		face_offset = Vector2i(clampi(value.x, 0, face_frames.x - 1), clampi(value.y, 0, face_frames.y - 1))
+		face_mesh.get_surface_override_material(0).set_shader_parameter("Offset", Vector2(face_offset.x, face_offset.y))
 
 @export var face_mesh : MeshInstance3D:
 	get:
-		return get_parent().get_child(0)
+		if face_mesh == null:
+			return get_parent().get_child(0)
+		return face_mesh
